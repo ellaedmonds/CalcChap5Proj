82a55634f3e9a6793a3dea117beceddadeb1d7d2
ellaedmonds <35499735+ellaedmonds@users.noreply.github.com>
2018-11-15 11:59:27 -0500

    Updated from Brython Server: 11/15/2018 11:59:25 AM

--- project.py
+++ project.py
@@ -85,48 +85,6 @@ xyderivzip=list(zip(xcoordlist, ycoordlist, derivlist1))
 extremalist=[]                              #here we find where d1 = 0
 increasinglist=[]                           #here we find the interval where it inc/dec
 decreasinglist=[]
-'''zero=[]
-b=-1
-c=1
-e=len(xyderivzip)
-print(e)
-for d in xyderivzip:
-    B=xyderivzip[b]
-    C=xyderivzip[c]
-    if d[2]>0:
-        increasinglist.append(d[0])
-    elif d[2]<0:
-        decreasinglist.append(d[0]) 
-    elif d[2]==0:
-        extremalist.append((d[0], d[1]))
-        increasinglist.append(d[0])
-        decreasinglist.append(d[0])
-        if B[2] < 0 and C[2] < 0:
-            print((d[0],d[1]),"is just a 0")
-        elif B[2] < 0 and C[2] > 0:
-            print((d[0],d[1]),"is a local min")
-        elif B[2] > 0 and C[2] < 0:
-            print((d[0],d[1]),"is a local max")
-        zero.append(d)
-    b+=1
-    c+=1
-    if c == e:
-        c=0
-        
-lengthincreasing=len(increasinglist)
-lengthdecreasing=len(decreasinglist)
-if lengthincreasing == 0:
-    print('Your function is never increasing')
-else:
-    print('Your function is increasing from',increasinglist[0],'to',increasinglist[-1])
-    
-if lengthdecreasing == 0:
-    print('Your function is never decreasing')
-else:
-    print('Your function is decreasing from',decreasinglist[0],'to',decreasinglist[-1])
-print()'''
-increasinglist=[]                           #here we find the interval where it inc/dec
-decreasinglist=[]
 zero = []
 b = -1
 c = 1
@@ -177,7 +135,6 @@ for d in xyderivzip:
         increasinglist.append(d[0])
         decreasinglist.append(d[0])
 
-#print ('the first derivative of your equation is equal to zero at:',extremalist)
 
 lengthincreasing=len(increasinglist)
 lengthdecreasing=len(decreasinglist)
